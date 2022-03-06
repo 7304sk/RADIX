c5281135cead512975d240c5f1cccdb07aaf8349
shoalwave <wave.ysk730@gmail.com>
2022-03-06 15:56:00 +0900

    ToggleNav, FlexFixの修正

--- js/index.js
+++ js/index.js
@@ -1,8 +1,3 @@
-// Font awesome のCSS疑似要素への許可
-window.FontAwesomeConfig = {
-    searchPseudoElements: true
-};
-
 // Radix js の初期化、追加スクリプト
 window.addEventListener('DOMContentLoaded', () => {
     let myRadix = new radix({
