e891185b1815a6379c35d7b358f9738f86b7dac0
shoalwave <wave.ysk730@gmail.com>
2022-03-29 20:04:20 +0900

    v4.0.0調整済み

--- js/index.js
+++ js/index.js
@@ -1,3 +1,6 @@
+// Font awesome のCSS疑似要素への使用を許可
+window.FontAwesomeConfig = { searchPseudoElements: true };
+
 window.addEventListener('DOMContentLoaded', () => {
     // Radix js の初期化、追加スクリプト
     let myRadix = new radix({
@@ -7,6 +10,9 @@ window.addEventListener('DOMContentLoaded', () => {
             target: '',
             preventScroll: false
         }
+    });
+    document.addEventListener('radixInit_', () => {
+        // 追加のスクリプトはこの中に記述する
     });
     myRadix.init();
 });
